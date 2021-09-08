62f98275eb34cbe25d2026c816d8e64e5bbb1a36
Davidfi34 <figuererodavid@gmail.com>
2021-09-08 20:27:33 -0300

    El programa terminado

--- index.php
+++ index.php
@@ -2,16 +2,26 @@
 
 echo "<h3>Encotrar la cantidad de numeros positivos</h3>";
 
-//funtion: genera numeros
+//funtion: genera numeros y devuelve los numeros positivos
 function genNum(){
     $cant = 20;
     $posit = 0;
     for($i=0; $i<$cant; $i++){
         $num = rand(-99,99);
         echo $num ." , ";
+        $posit = NumPost($num,$posit);
     }
     return $posit;
 }
+
+//funcion: validacion de numeros positivos
+function NumPost($num,$posit){
+    if($num >= 0){
+        ++$posit;
+    }
+    return $posit;
+}
+
 
 $Resultado = genNum();
 echo "<br>";
