d28e9fe41c3a0faecb95107e9004926468dca8a8
Davidfi34 <figuererodavid@gmail.com>
2021-09-08 20:12:53 -0300

    a mitad de camino

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
-/*En este programa se generan 20 números al azar y luego muestra la cantidad de números positivos encontrados*/ 
+echo "<h3>Encotrar la cantidad de numeros positivos</h3>";
 
+//funtion: genera numeros
+function genNum(){
+    $cant = 20;
+    $posit = 0;
+    for($i=0; $i<$cant; $i++){
+        $num = rand(-99,99);
+        echo $num ." , ";
+    }
+    return $posit;
+}
+
+$Resultado = genNum();
+echo "<br>";
+echo "El resultado es: ". $Resultado;
+    
 ?>
